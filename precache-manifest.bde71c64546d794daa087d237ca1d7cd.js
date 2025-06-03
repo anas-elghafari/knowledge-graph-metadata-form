dc022df6494ef0121d693127d3e03518b592f85a
anas-elghafari <anas.elghafari@gmail.com>
2025-06-03 07:30:42 +0300

    Updates

---
+++ precache-manifest.bde71c64546d794daa087d237ca1d7cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ba95c96b4fb82a1c10990b29ce22379",
+    "url": "/knowledge-graph-metadata-form/index.html"
+  },
+  {
+    "revision": "72ffda51110ee5043287",
+    "url": "/knowledge-graph-metadata-form/static/css/main.22e1e9dc.chunk.css"
+  },
+  {
+    "revision": "6593f894d016e85388cb",
+    "url": "/knowledge-graph-metadata-form/static/js/2.c7821bdd.chunk.js"
+  },
+  {
+    "revision": "ea935f47a7b92aa14dc7",
+    "url": "/knowledge-graph-metadata-form/static/js/3.a29efab2.chunk.js"
+  },
+  {
+    "revision": "72ffda51110ee5043287",
+    "url": "/knowledge-graph-metadata-form/static/js/main.3bfec11f.chunk.js"
+  },
+  {
+    "revision": "a05ea10eb52b924ea758",
+    "url": "/knowledge-graph-metadata-form/static/js/runtime~main.5695be88.js"
+  }
+]);
